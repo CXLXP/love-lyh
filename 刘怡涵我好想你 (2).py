446e09ddbc00a03396134d690112b09bf02f0c91
CXLYH <chenxiaolong2005@outlook.com>
2025-10-22 00:54:03 +0800

    Add files via upload

---
+++ 刘怡涵我好想你 (2).py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import random
+import threading
+import time
+
+def dow():
+    window = tk.Tk()
+    width = window.winfo_screenwidth()
+    height = window.winfo_screenheight()
+    a = random.randrange(0, width)
+    b = random.randrange(0, height)
+    
+    window.title("涵")
+    window.geometry("220x50+" + str(a) + "+" + str(b))
+    
+    tk.Label(window,
+             text='我想你了！',
+             bg='pink',
+             font=('楷体', 18),
+             width=10,
+             height=1
+             ).pack()
+    window.mainloop()
+
+threads = []
+for i in range(200):
+    t = threading.Thread(target=dow)
+    threads.append(t)
+    time.sleep(0.01)
+    threads[i].start()
